845ce451cdee481477067a2c99eac423ee3f88cb
Jared Meek <jared.meek@columbia.edu>
2021-04-12 11:11:37 -0400

    rename capcomm.py to map.py

--- mountaineer/capcomm.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-
-"""
-A program to specify polygon or multipolygon shape and infer ecological
-community structure based on species occurrence records.
-"""
---
+++ mountaineer/map.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+
+"""
+Map downloaded occurrence records within polygons.
+"""
